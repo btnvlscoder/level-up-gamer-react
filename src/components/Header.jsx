27c57d043e30d4c3287537f553ff6fdb4157d8b1
btnvlscoder <btnvlscoder@gmail.com>
2025-09-28 18:26:56 -0300

    Se integra css de layout; btn y centrado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export const Header = () => {
     return(
-        <header>
-            <a href="/">
-            <h1 className="logo">
-                <i className="bi bi-joystick"></i> Level-Up Gamer
-            </h1>
-            </a>
-            <img className="logojpg" src="/logo.jpg" alt="Logo Level-Up Gamer"/>
-        </header>
+    <header>
+        <a href="/">
+        <h1 className="logo">
+            <i className="bi bi-joystick"></i> Level-Up Gamer
+        </h1>
+        </a>
+        <img className="logojpg" src="/logo.jpg" alt="Logo Level-Up Gamer"/>
+    </header>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,22 +10,22 @@ export const Layout = ({ children, activePage = 'home' }) => {
         <nav>
           <ul className="menu">
             <li>
-              <a className={`boton-menu ${activePage === 'home' ? 'active' : ''}`} href="/">
+              <a className={`btn-menu ${activePage === 'home' ? 'active' : ''}`} href="/">
                 <i className="bi bi-house-door"></i> Inicio
               </a>
             </li>
             <li>
-              <a className={`boton-menu ${activePage === 'products' ? 'active' : ''}`} href="/productos">
+              <a className={`btn-menu ${activePage === 'products' ? 'active' : ''}`} href="/productos">
                 <i className="bi bi-controller"></i> Productos
               </a>
             </li>
             <li>
-              <a className={`boton-menu ${activePage === 'contact' ? 'active' : ''}`} href="/contacto">
+              <a className={`btn-menu ${activePage === 'contact' ? 'active' : ''}`} href="/contacto">
                 <i className="bi bi-chat-dots-fill"></i> Contactanos
               </a>
             </li>
             <li>
-              <a className={`boton-menu boton-carrito ${activePage === 'cart' ? 'active' : ''}`} href="/carrito">
+              <a className={`btn-menu btn-carrito ${activePage === 'cart' ? 'active' : ''}`} href="/carrito">
                 <i className="bi bi-cart"></i> Carrito <span className="numerito">0</span>
               </a>
             </li>
